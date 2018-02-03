57e7640ce24cd87686b6413523f55776297baa41
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-03 08:24:55 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   index = 0
   while index < collection.length
-    return collection[i] if yield(collection[i])
+    return collection[index] if yield(collection[index])
       index += 1
     end
   end
